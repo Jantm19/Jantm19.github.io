3d56ae5df0f5e8bbcde2ba92bbaa1bcfaacca6d2
Jantm19 <84690812+Jantm19@users.noreply.github.com>
2023-07-26 19:32:24 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('Ingeniera en Computación | Tecnico en informatica | Analista de datos')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
